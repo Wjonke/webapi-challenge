4370b61b438caa47c8bdd578c7c7f3bbde4b5620
Wes Jonke <wesjonke@gmail.com>
2019-08-09 14:58:37 -0400

    modified routes

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const choreRouter = require('./chores/choreRouter');
 const port = process.env.PORT || 8000;
 
 const server = express();
-
+//custom middleware
+server.use(express.json())
 
 server.get('/api',  (req, res) => {
   res.send(`<h2>Deployed</h2>`)
 });
 
-//custom middleware
-server.use(express.json())
+
 
 //routes
-server.use("/api", choreRouter)
+
 
 server.listen(port, ()=> console.log(` server running on ${port} `))
