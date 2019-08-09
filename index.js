40e99235186fafafcb199d19cc13c516698e8985
Wes Jonke <wesjonke@gmail.com>
2019-08-09 15:08:21 -0400

    modified routes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const choreRouter = require('./chores/choreRouter');
+const Chores = require("./data/data");
+const People = require("./data/data");
 
 const port = process.env.PORT || 8000;
 
 const server = express();
-//custom middleware
+
 server.use(express.json())
 
 
